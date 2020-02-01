4366a29477861fc66479a262cfa6124cec0be9b9
jkbishay <32003122+jkbishay@users.noreply.github.com>
2020-02-01 10:36:47 -1000

    push WEEB

---
+++ Assets/Scripts/WEEBControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WEEBControl : MonoBehaviour
+{
+    public GameObject Waifu;
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(Waifu.transform.position);
+    }
+}
